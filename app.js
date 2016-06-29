9e6dba7d4d61ef59562ff232c2f0a4ed6ed649cf
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-06-29 22:17:58 +0700

    Implements peoplesCount

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.controller('summaryController',function summaryController($scope, $http){
 	$http.get('http://localhost:3000/api/genders/count/').then(function(response){
 		$scope.gendersCount = response.data[0].count;
 	});
+
+	$http.get('http://localhost:3000/api/peoples/count/').then(function(response){
+		$scope.peoplesCount = response.data[0].count;
+	});
 	
 });
