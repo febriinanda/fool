7ce09c34478894cb7659b0cb0ac13d864bc8d2c2
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-06-30 05:13:33 +0700

    Add some form and formController

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ app.controller('summaryController',function summaryController($scope, $http){
 	});
 	
 });
+
+app.controller('formController',function formController($scope, $http){
+	$http.get('http://localhost:3000/api/religions').then(function(response){
+		$scope.religions = response.data;
+	});
+
+	$http.get('http://localhost:3000/api/genders').then(function(response){
+		$scope.genders = response.data;
+	});
+
+	this.formData = {};
+
+	this.addData = function(){
+		console.log(this.formData);
+		this.formData = {};
+	}
+});
